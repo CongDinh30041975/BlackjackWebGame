ccb6a8938ef9410cd7c50d80e55ae99565d887ac
Bùi Công Định <kinhdong0556@gmail.com>
2026-02-03 15:27:54 +0700

    Them room service va room state

---
+++ src/components/Play/ChoosePublicRooms.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ChoosePublicRooms = () => {
+  return (
+    <div>
+      <button>Phòng công khai</button>
+      <button>Phòng riêng</button>
+    </div>
+  )
+}
+
+export default ChoosePublicRooms
